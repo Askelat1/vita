38df66151562b342ad7e7354b086efd9ac545bc5
Isssaqek <125139031+Isssaqek@users.noreply.github.com>
2023-03-20 21:33:05 +0600

    Add files via upload

--- src/pages/Tovar_2.jsx
+++ src/pages/Tovar_2.jsx
@@ -2,6 +2,8 @@ import module from './Tovar_2.module.css'
 import React from 'react';
 import vita from '../assets/vin1.png'
 import fon from '../assets/fon.png'
+import like from '../assets/love.png'
+import shop from '../assets/shop_2.png'
 
 
 function Tovar_2 (props) {
@@ -19,33 +21,27 @@ function Tovar_2 (props) {
                         <div className={module.infor}>подробнее</div>
                     </div>
                 </div>
-            </div>
-            {/* <div className={module.text}>
-            <img className={module.vita} src={vita} />
-            <div className={module.block}>
-                <h1 className={module.h1}>Витамины для волос 60 капс</h1>
-                <div className={module.p}>
-                    <p>
-                Улучшает микроциркуляцию и энергообеспечение волосяных фолликулов, препятствуя их выпадению.Стимулирует синтез коллагена и   кератина, способствует росту волос.</p></div>
-                <div className={module.like}>
-                    <div className={module.kol}>
-                    <h3 className={module.h3}>кол-во:</h3>
-                        <div className={module.up}>
-                        <i class="fa-solid fa-angle-up"></i>
-                        <i class="fa-solid fa-angle-down"></i>
+                <div className={module.kol}>
+                        <div className={module.kol_box}>
+                            <h3 className={module.h3}>кол-во:</h3>
+                            <div className={module.up}>
+                                <i class="fa-solid fa-angle-up"></i>
+                                <i class="fa-solid fa-angle-down"></i>
+                            </div>
                         </div>
-                    </div>
-                    <div className={module.likes}><i class="fa-sharp fa-solid fa-heart">
-                    <i class="fa-regular fa-bag-shopping"></i> 
-                    </i>
-                    </div>
+                        <div className={module.icon}>
+                            <img className={module.icon_like} src={like}></img>
+                            <img className={module.icon_shop} src={shop}></img>
+                        </div>
                 </div>
-                
-                <div className={module.karta2}>
-                <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
-                </div>
+                <div className={module.form}>
+                            <div className={module.karta2}>
+                                <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
+                            </div>
+                        </div>
             </div>
-            </div> */}
+            
+            
         </div>
         
 
