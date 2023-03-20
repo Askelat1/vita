c0a5624c01a06726e7df67cc5786a85ead3d7dc2
Askelat1 <114641854+Askelat1@users.noreply.github.com>
2023-03-20 21:23:17 +0600

    Add files via upload

--- src/pages/Tovar_2.jsx
+++ src/pages/Tovar_2.jsx
@@ -1,6 +1,6 @@
 import module from './Tovar_2.module.css'
 import React from 'react';
-import vita1 from '../assets/vin1.png'
+import vita from '../assets/vin1.png'
 import fon from '../assets/fon.png'
 import like from '../assets/love.png'
 import shop from '../assets/shop_2.png'
@@ -10,7 +10,7 @@ function Tovar_2 (props) {
 
     return (
         <div className={module.fon}>
-            {/* <div className={module.block}>
+            <div className={module.block}>
                 <div className={module.main_text}> Витамины для волос <br /> 60 капс</div>
                 <div className={module.container}>
                     <div className={module.cont_1}>
@@ -33,42 +33,15 @@ function Tovar_2 (props) {
                             <img className={module.icon_like} src={like}></img>
                             <img className={module.icon_shop} src={shop}></img>
                         </div>
-                        
-                    </div>
-                    <div className={module.form}>
-                        <div className={module.karta2}>
-                            <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
-                        </div>
                 </div>
-                
-            </div> */}
+                <div className={module.form}>
+                            <div className={module.karta2}>
+                                <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
+                            </div>
+                        </div>
+            </div>
             
-            <div className={module.text}>
-            <img className={module.vita} src={vita} />
-            <div className={module.block}>
-                <h1 className={module.h1}>Витамины для волос 60 капс</h1>
-                <div className={module.p}>
-                    <p>
-                Улучшает микроциркуляцию и энергообеспечение волосяных фолликулов, препятствуя их выпадению.Стимулирует синтез коллагена и   кератина, способствует росту волос.</p></div>
-                <div className={module.like}>
-                    <div className={module.kol}>
-                    <h3 className={module.h3}>кол-во:</h3>
-                        <div className={module.up}>
-                        <i class="fa-solid fa-angle-up"></i>
-                        <i class="fa-solid fa-angle-down"></i>
-                        </div>
-                    </div>
-                    <div className={module.likes}><i class="fa-sharp fa-solid fa-heart">
-                    <i class="fa-regular fa-bag-shopping"></i> 
-                    </i>
-                    </div>
-                </div>
-                
-                <div className={module.karta2}>
-                <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
-                </div>
-            </div>
-            </div>
+            
         </div>
         
 
