be7725d1a680ff0885a344d21d3c7fa4225792cb
Isssaqek <125139031+Isssaqek@users.noreply.github.com>
2023-03-20 19:10:11 +0600

    Add files via upload

--- src/pages/Tovar_2.jsx
+++ src/pages/Tovar_2.jsx
@@ -1,12 +1,26 @@
 import module from './Tovar_2.module.css'
 import React from 'react';
-import vita from '../assets/vitamin.png'
+import vita from '../assets/vin1.png'
+import fon from '../assets/fon.png'
+
 
 function Tovar_2 (props) {
 
     return (
         <div className={module.fon}>
-            <div className={module.text}>
+            <div className={module.block}>
+                <div className={module.main_text}> Витамины для волос <br /> 60 капс</div>
+                <div className={module.container}>
+                    <div className={module.cont_1}>
+                        <div className={module.cont_1_img}></div>
+                    </div>
+                    <div className={module.cont_2}>
+                        <div className={module.cont_2_text}>Улучшает микроциркуляцию и энергообеспечение волосяных фолликулов, препятствуя их выпадению.Стимулирует синтез коллагена и кератина, способствует росту волос.</div>
+                        <div className={module.infor}>подробнее</div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className={module.text}>
             <img className={module.vita} src={vita} />
             <div className={module.block}>
                 <h1 className={module.h1}>Витамины для волос 60 капс</h1>
@@ -31,9 +45,9 @@ function Tovar_2 (props) {
                 <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
                 </div>
             </div>
-            
+            </div> */}
         </div>
-        </div>
+        
 
     )
 }
