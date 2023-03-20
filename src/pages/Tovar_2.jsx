2c743f72639dbcfb8d30a9e1cc36a9b2cd9251f0
Askelat1 <114641854+Askelat1@users.noreply.github.com>
2023-03-20 21:16:01 +0600

    Add files via upload

--- src/pages/Tovar_2.jsx
+++ src/pages/Tovar_2.jsx
@@ -33,42 +33,15 @@ function Tovar_2 (props) {
                             <img className={module.icon_like} src={like}></img>
                             <img className={module.icon_shop} src={shop}></img>
                         </div>
-                        
-                    </div>
-                    <div className={module.form}>
-                        <div className={module.karta2}>
-                            <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
-                        </div>
                 </div>
-                
+                <div className={module.form}>
+                            <div className={module.karta2}>
+                                <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
+                            </div>
+                        </div>
             </div>
             
-            {/* <div className={module.text}>
-            <img className={module.vita} src={vita} />
-            <div className={module.block}>
-                <h1 className={module.h1}>Витамины для волос 60 капс</h1>
-                <div className={module.p}>
-                    <p>
-                Улучшает микроциркуляцию и энергообеспечение волосяных фолликулов, препятствуя их выпадению.Стимулирует синтез коллагена и   кератина, способствует росту волос.</p></div>
-                <div className={module.like}>
-                    <div className={module.kol}>
-                    <h3 className={module.h3}>кол-во:</h3>
-                        <div className={module.up}>
-                        <i class="fa-solid fa-angle-up"></i>
-                        <i class="fa-solid fa-angle-down"></i>
-                        </div>
-                    </div>
-                    <div className={module.likes}><i class="fa-sharp fa-solid fa-heart">
-                    <i class="fa-regular fa-bag-shopping"></i> 
-                    </i>
-                    </div>
-                </div>
-                
-                <div className={module.karta2}>
-                <button className={module.karta}>оформить сейчас <i class="fa-solid fa-arrow-right"></i></button>
-                </div>
-            </div>
-            </div> */}
+            
         </div>
         
 
